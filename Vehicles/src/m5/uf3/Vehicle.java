112ea1888d0e74b1b7964fecda1ce541edf44153
Carlos <Carlos@192.168.1.43>
2018-04-15 12:19:06 +0200

    Clase inicial vehicle

---
+++ Vehicles/src/m5/uf3/Vehicle.java
@@ -0,0 +1,31 @@
+package m5.uf3;
+
+public class Vehicle {
+	
+	public static final double velocitat_maxima=120;
+	
+	private int cilindrada;
+	private int portes;
+	private double velocitat_actual;
+	
+	public Vehicle(double velocitat_actual) {
+		super();
+		this.velocitat_actual = velocitat_actual;
+	}
+	
+	public double getVelocitat() {
+		return velocitat_actual;
+	}
+
+	public void setVelocitat(double velocitat_actual) {
+		this.velocitat_actual = velocitat_actual;
+	}
+
+	public double accelerar(double velocitat_actual){
+		if(this.velocitat_actual<velocitat_maxima)
+			return this.velocitat_actual+=10;
+		else
+			return this.velocitat_actual;
+	}
+
+}
